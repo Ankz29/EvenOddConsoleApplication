d0abe8b2d828f6cda5efeb06ecfd510d02269336
ankita_muddapur <ankita_muddapur@cms.co.in>
2020-03-11 13:07:57 +0530

    Adding files for Even and Odd Number.

---
+++ EvenOddUtility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOddConsoleApplication
+{
+      public static class EvenOddUtility
+    {
+        public static string EvenOddMethod(int value)
+        {
+            if(value %2 == 0)
+            {
+                return "Entered value is Even: "+ value;
+            }
+            else
+            {
+                return "Entered value is Odd: "+ value;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EvenOddConsoleApplication;
+
+namespace EvenOddConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write( "Enter the value to check for even and odd value: " );
+            var Number = Convert.ToInt32(Console.ReadLine());
+
+            var EvenOdd = EvenOddUtility.EvenOddMethod( Number );
+            Console.Write( EvenOdd );
+            Console.ReadLine();
+        }
+    }
+}
